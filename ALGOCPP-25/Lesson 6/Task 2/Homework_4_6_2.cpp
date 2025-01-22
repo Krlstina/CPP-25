4697c8ab9b98a818ad4b8384da1d69b323a9ca3f
Kristina Pankina <pankina_k00@mail.ru>
2025-01-22 19:11:09 +0300

    Adding Homework ALGOCPP-25 Lesson 6

---
+++ ALGOCPP-25/Lesson 6/Task 2/Homework_4_6_2.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <string>
+
+int real_string_hash(std::string word, int primeNumber, int module)
+{
+    long long stringHash{};
+
+    for (int i = 0; i < word.length(); ++i)
+    {
+        stringHash += word[i] * pow(primeNumber, i);
+    }
+
+    return stringHash % module;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    std::string enterLine{};
+    int prime{};
+    int module{};
+
+    std::cout << "Введите р: ";
+    std::cin >> prime;
+    std::cout << "Введите n: ";
+    std::cin >> module;
+
+    do
+    {
+        std::cout << "Введите строку: ";
+        std::cin >> enterLine;
+        std::cout << "Наивный хеш строки " << enterLine << " = " << real_string_hash(enterLine, prime, module) << std::endl;
+    } while (enterLine != "exit");
+
+    return EXIT_SUCCESS;
+}
