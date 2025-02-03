eaf8ebe11565237b7e3a6527fe0a57f6f80e9387
Kristina Pankina <pankina_k00@mail.ru>
2025-02-03 22:12:08 +0300

    Adding Homework CPPL-25 Lesson 1

---
+++ CPPL-25/Lesson 1/Task 1/Homework_5_1_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+struct Point {
+	double m_x{};
+	double m_y{};
+
+	Point(double x, double y) {
+		this->m_x = x;
+		this->m_y = y;
+	}
+};
+
+void print_point(const Point& point_object) {
+	cout << "x:" << point_object.m_x << ", y:" << point_object.m_y << std::endl;
+}
+
+int main()
+{
+	Point my_point{0.0, 0.0};
+
+	for (int i = 0; i < 5; i++)
+	{
+		my_point.m_x = i;
+		my_point.m_y = 2 * i;
+		print_point(my_point);
+	}
+
+	return EXIT_SUCCESS;
+}
